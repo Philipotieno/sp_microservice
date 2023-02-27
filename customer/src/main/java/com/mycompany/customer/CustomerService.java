d63b63c1227e44bc796ac754d6eda5d128f3a9f8
Philip Otieno <iamphilipotieno@gmail.com>
2023-02-27 22:33:28 +0300

    Add customer

--- customer/src/main/java/com/mycompany/customer/CustomerService.java
+++ customer/src/main/java/com/mycompany/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.mycompany.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +12,6 @@ public record CustomerService() {
                 .build();
         // todo: check if email is valid
         // todo: check if email is unique/not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 }
